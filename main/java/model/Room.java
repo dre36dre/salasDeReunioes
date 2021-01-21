939d0212426432cce386db3042141b159db160bc
dre36dre <dre.son36@gmail.com>
2021-01-21 00:24:35 -0300

    Criação da classe Room na Model

--- main/java/model/Room.java
+++ main/java/model/Room.java
@@ -1,5 +1,47 @@
 package model;
 
-public class Room {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="meetingroom")
+public class Room {
+	
+	private String name;
+	private String date;
+	private String startHour;
+	private String endHour; 
+	
+	private long id;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getStartHour() {
+		return startHour;
+	}
+	public void setStartHour(String startHour) {
+		this.startHour = startHour;
+	}
+	public String getEndHour() {
+		return endHour;
+	}
+	public void setEndHour(String endHour) {
+		this.endHour = endHour;
+	}
+	
 }
