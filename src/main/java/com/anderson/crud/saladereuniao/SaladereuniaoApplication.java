30a1c7362d744ed2786e3457b858e2c3c29d9a41
dre36dre <dre.son36@gmail.com>
2021-02-10 00:47:40 -0300

    falta arquivo html

--- src/main/java/com/anderson/crud/saladereuniao/SaladereuniaoApplication.java
+++ src/main/java/com/anderson/crud/saladereuniao/SaladereuniaoApplication.java
@@ -1,5 +1,5 @@
 package com.anderson.crud.saladereuniao;
-
+//falta os arquivos.html
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
